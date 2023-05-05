b71572264e68156f6ef1fbfc262646a09afc1889
jarry <acbbc70412@gmail.com>
2023-05-05 10:33:35 +0800

    git init

--- pages/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Header_1 = require("@/components/Header");
-function Home() {
-    return (React.createElement(React.Fragment, null,
-        React.createElement(Header_1["default"], { label: 'Home', showBackArrow: false })));
-}
-exports["default"] = Home;
